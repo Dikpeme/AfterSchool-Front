11482a74de810448fd342a3eb6c72fe14f06f6b7
Dikpeme <72168168+Dikpeme@users.noreply.github.com>
2020-11-20 17:59:48 +0100

    Add files via upload

---
+++ src/components/Popupcarty.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="box">
+<h2>Checkout Success</h2>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import btn from './Btn';
+
+export default {
+  components: {
+    btn,
+  },
+  methods: {
+    ...mapActions([
+      'showOrHiddenPopupCarty',
+    ]),
+    showPopupCarty() {
+      this.showOrHiddenPopupCarty();
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .box {
+    width: 400px;
+    height: auto;
+    background-color: #FAFAFA;
+    box-shadow: 0px 0px 10px rgba(73, 74, 78, 0.1);
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: 1em .5em;
+    position: absolute;
+    z-index: 1;
+  }
+
+  .box:after {
+    content: '';
+    width: 30px;
+    height: 30px;
+    transform: rotate(45deg);
+    background: inherit;
+    position: absolute;
+    top: -15px;
+    right: 15px;
+  }
+
+  .box-item {
+    width: 100%;
+    height: 130px;
+    background-color: #fff;
+    box-sizing: border-box;
+    border-radius: 3px;
+    padding: 0 .5em;
+    margin-top: .3em;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  .item-thumb {
+    max-width: 70%;
+    grid-column: 1/2;
+    grid-row: 1/4;
+    align-self: center;
+  }
+
+  .item-name {
+    grid-column: 2/4;
+    grid-row: 1/2;
+    font-weight: normal;
+  }
+
+  .item-amount {
+    grid-column: 2/3;
+    grid-row: 2/4;
+    color: #ddd;
+  }
+
+  .cart-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+</style>
